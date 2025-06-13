eea729400cb8ebefea88b43ee7e2474afd35ee86
Miguel Miguez <138310942+MiguelMiguez@users.noreply.github.com>
2025-06-13 16:55:40 -0300

    Update
    -Se agregaron Dashboards en el home para visualizar información.

--- src/app/home/Home.tsx
+++ src/app/home/Home.tsx
@@ -3,8 +3,12 @@ import BentoHome from "../../components/BentoHome/BentoHome";
 import IconUserBlue from "../../assets/icons/user blue.svg";
 import IconCarBlue from "../../assets/icons/car blue.svg";
 import IconSettingsBlue from "../../assets/icons/security blue.svg";
+import DashboardMetrics from "../../components/DashboardMetrics/DashboardMetrics";
+import { useNavigate } from "react-router-dom";
 
 export default function Home() {
+  const navigate = useNavigate();
+
   return (
     <section className="home">
       <div className="home-content">
@@ -12,9 +16,16 @@ export default function Home() {
           <h1>Bienvenido @User </h1>
         </div>
         <div className="bento-home-container">
-          <BentoHome icon={IconUserBlue} text="Gestión de Usuarios" />
+          <BentoHome
+            icon={IconUserBlue}
+            text="Gestión de Usuarios"
+            onClick={() => navigate("/user")}
+          />
           <BentoHome icon={IconCarBlue} text="Gestión de Vehículos" />
           <BentoHome icon={IconSettingsBlue} text="Gestión de Permisos" />
+        </div>
+        <div>
+          <DashboardMetrics />
         </div>
       </div>
     </section>
